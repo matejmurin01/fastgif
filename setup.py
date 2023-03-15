7be0a0846452a71e8eb8820ae9712bdf6b660b79
Matej Murin <matejmurin01@gmail.com>
2023-03-15 12:42:44 +0100

    Changed setup to new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="fastgif",
-    version="1.1",
+    version="2.0",
     description="Matplotlib GIF maker",
     long_description=long_description,
     long_description_content_type="text/markdown",
