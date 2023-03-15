7afcfb09ba0b016a9b574813bd0ad9bc9cca8db7
Matej Murin <matejmurin01@gmail.com>
2023-03-15 12:42:24 +0100

    Added helpers for swapping and moving gif

--- fastgif.py
+++ fastgif.py
@@ -6,10 +6,35 @@ import imageio
 import matplotlib.pyplot as plt
 import multiprocessing as mp
 from matplotlib.figure import Figure
-from typing import Callable, Iterable
+from typing import Callable, Iterable, List, Dict, Tuple, Any
+
+__version__ = 2.0
 
 
-__version__ = 1.1
+class PreparedOutput:
+    def __init__(self,
+                 data: Any,
+                 title: str = None,
+                 title_fontsize: int = 10,
+                 x_label: str = None,
+                 x_label_fontsize: int = 10,
+                 y_label: str = None,
+                 y_label_fontsize: int = 10,
+                 x_ticks: list = None,
+                 x_ticks_fontsize: int = 10,
+                 y_ticks: list = None,
+                 y_ticks_fontsize: int = 10):
+        self.data = data
+        self.title = title
+        self.title_fontsize = title_fontsize
+        self.x_label = x_label
+        self.x_label_fontsize = x_label_fontsize
+        self.y_label = y_label
+        self.y_label_fontsize = y_label_fontsize
+        self.x_ticks = x_ticks
+        self.x_ticks_fontsize = x_ticks_fontsize
+        self.y_ticks = y_ticks
+        self.y_ticks_fontsize = y_ticks_fontsize
 
 
 def __worker_fn(chunk: Iterable, where: str, fig_fn: Callable[[int], Figure]) -> None:
@@ -84,3 +109,123 @@ def make_gif(
             shutil.rmtree(where)
         except FileNotFoundError:
             pass
+
+
+def swapping_gif(data: List[Dict[Tuple[int, int], PreparedOutput]],
+                 filename: str,
+                 n_rows: int,
+                 n_cols: int,
+                 num_processes: int = 1,
+                 suptitles: List[str] = None,
+                 suptitle_fontsize: int = 10,
+                 show_progress: bool = False,
+                 tight_layout: bool = False,
+                 figure_kwargs: dict = None,
+                 plot_kwargs: dict = None,
+                 writer_kwargs: dict = None):
+    if figure_kwargs is None:
+        figure_kwargs = {
+            'figsize': (10, 10),
+            'dpi': 150,
+            'facecolor': 'white'
+        }
+    if plot_kwargs is None:
+        plot_kwargs = {
+            'vmin': 0,
+            'vmax': 1,
+            'cmap': plt.cm.nipy_spectral
+        }
+    global gif_fn
+
+    def gif_fn(idx):
+        fig, ax = plt.subplots(n_rows, n_cols, **figure_kwargs)
+
+        for row in range(n_rows):
+            for col in range(n_cols):
+                plot_data = data[idx][(row, col)]
+                ax[row, col].imshow(plot_data.data, **plot_kwargs)
+                if plot_data.title is not None:
+                    ax[row, col].set_title(plot_data.title, fontsize=plot_data.title_fontsize)
+                if plot_data.x_label is not None:
+                    ax[row, col].set_xlabel(plot_data.x_label, fontsize=plot_data.x_label_fontsize)
+                if plot_data.y_label is not None:
+                    ax[row, col].set_ylabel(plot_data.y_label, fontsize=plot_data.y_label_fontsize)
+                if plot_data.x_ticks is not None:
+                    ax[row, col].set_xticks(plot_data.x_ticks, fontsize=plot_data.x_ticks_fontsize)
+                if plot_data.y_ticks is not None:
+                    ax[row, col].set_yticks(plot_data.y_ticks, fontsize=plot_data.y_ticks_fontsize)
+
+        if suptitles is not None:
+            fig.suptitle(suptitles[idx], fontsize=suptitle_fontsize)
+
+        if tight_layout:
+            fig.tight_layout()
+
+        return fig
+
+    make_gif(gif_fn, len(data), filename, num_processes, show_progress=show_progress,
+             writer_kwargs=writer_kwargs)
+
+
+def moving_gif(data: Dict[Tuple[int, int], PreparedOutput],
+               filename: str,
+               n_rows: int,
+               n_cols: int,
+               num_steps: int,
+               num_processes: int = 1,
+               timestamps: List[str] = None,
+               suptitles: List[str] = None,
+               suptitle_fontsize: int = 10,
+               show_progress: bool = False,
+               tight_layout: bool = False,
+               figure_kwargs: dict = None,
+               plot_kwargs: dict = None,
+               writer_kwargs: dict = None):
+    if figure_kwargs is None:
+        figure_kwargs = {
+            'figsize': (10, 10),
+            'dpi': 150,
+            'facecolor': 'white'
+        }
+    if plot_kwargs is None:
+        plot_kwargs = {
+            'vmin': 0,
+            'vmax': 1,
+            'cmap': plt.cm.nipy_spectral
+        }
+    global gif_fn
+
+    def gif_fn(idx):
+        fig, ax = plt.subplots(n_rows, n_cols, **figure_kwargs)
+
+        for row in range(n_rows):
+            for col in range(n_cols):
+                if (row, col) not in data:
+                    continue
+                plot_data = data[(row, col)]
+                ax[row, col].imshow(plot_data.data[idx], **plot_kwargs)
+                if timestamps is not None:
+                    tstep_txt = ' ' + timestamps[idx]
+                else:
+                    tstep_txt = ''
+
+                if plot_data.title is not None:
+                    ax[row, col].set_title(plot_data.title + tstep_txt, fontsize=plot_data.title_fontsize)
+                if plot_data.x_label is not None:
+                    ax[row, col].set_xlabel(plot_data.x_label, fontsize=plot_data.x_label_fontsize)
+                if plot_data.y_label is not None:
+                    ax[row, col].set_ylabel(plot_data.y_label, fontsize=plot_data.y_label_fontsize)
+                if plot_data.x_ticks is not None:
+                    ax[row, col].set_xticks(plot_data.x_ticks, fontsize=plot_data.x_ticks_fontsize)
+                if plot_data.y_ticks is not None:
+                    ax[row, col].set_yticks(plot_data.y_ticks, fontsize=plot_data.y_ticks_fontsize)
+
+        if suptitles is not None:
+            fig.suptitle(suptitles[idx], fontsize=suptitle_fontsize)
+
+        if tight_layout:
+            fig.tight_layout()
+
+        return fig
+
+    make_gif(gif_fn, num_steps, filename, num_processes, show_progress=show_progress, writer_kwargs=writer_kwargs)
